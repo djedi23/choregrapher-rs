b06162805cd048fdc2ed6a086170db4de6eab496
Moïse Valvassori <moise.valvassori@gmail.com>
2023-02-18 17:33:34 +0100

    restorore OutputProcessing test

--- node-rs/src/output_processor.rs
+++ node-rs/src/output_processor.rs
@@ -10,7 +10,7 @@ pub trait OutputProcessing {
 
 // type OutputProcessorFunction<T, U> =
 //   Box<dyn Fn(T, Arc<Context>) -> Vec<(U, Arc<Context>)> + Clone + Sync + Send>;
-/// Default output processor just convert I to [I]
+/// Default output processor just convert I to \[I\]
 #[derive(Clone)]
 pub struct DefaultOutputProcessor<I, O> {
   input: PhantomData<I>,
@@ -36,38 +36,39 @@ impl<I: Clone, O: From<I> + Clone> OutputProcessing for DefaultOutputProcessor<I
   }
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-//   use crate::{
-//     context::FlowContext,
-//     graph::{InputRef, OutputRef, Relation},
-//   };
-//   use std::{collections::HashMap, sync::Mutex};
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::{
+    context::FlowContext,
+    graph::{InputRef, OutputRef, Relation},
+  };
+  use std::{collections::HashMap, sync::Mutex};
 
-//   #[test]
-//   fn default() {
-//     let op: DefaultOutputProcessor<i32, i32> = DefaultOutputProcessor::default();
-//     let context = Arc::new(Context {
-//       process_id: "id".into(),
-//       relation: Relation {
-//         from: OutputRef {
-//           node: "a".to_string(),
-//           output: "o".to_string(),
-//         },
-//         to: InputRef {
-//           node: "n".to_string(),
-//           input: "i".to_string(),
-//         },
-//       },
-//       context: FlowContext {
-//         labels: Arc::new(Mutex::new(HashMap::new())),
-//         context: Arc::new(Mutex::new(HashMap::new())),
-//       },
-//     });
+  #[test]
+  fn default() {
+    let op: DefaultOutputProcessor<i32, i32> = DefaultOutputProcessor::default();
+    let context = Arc::new(Context {
+      process_id: "id".into(),
+      relation: Relation {
+        from: OutputRef {
+          node: "a".to_string(),
+          output: "o".to_string(),
+        },
+        to: InputRef {
+          node: "n".to_string(),
+          input: "i".to_string(),
+        },
+      },
+      context: FlowContext {
+        labels: Arc::new(Mutex::new(HashMap::new())),
+        context: Arc::new(Mutex::new(HashMap::new())),
+      },
+      app: None,
+    });
 
-//     let res = op.process(4, context);
+    let res = op.process(4, context);
 
-//     assert_eq!(res[0].0, 4);
-//   }
-// }
+    assert_eq!(res[0].0, 4);
+  }
+}
